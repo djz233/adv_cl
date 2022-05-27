7260381727c76c0307d95f72e5581b9069ff9ebd
djz233 <49532599+djz233@users.noreply.github.com>
2022-05-27 12:48:22 +0800

    Update task_utils.py
    多数据集支持

--- task_utils.py
+++ task_utils.py
@@ -23,11 +23,61 @@ task_dict = {
         'num_label':2,
         'label_ids':{'positive':0, 'negative':1},
         'max_len':512,
-        'template':'the movie is <mask>, <sent>',
-        'label_words':[['Ġfantastic', 'Ġwonderful'], ['Ġterrible', 'Ġbad']],
+        'template': '<sent0> In summary, the film was <mask>.',
+        #'template':'the movie is <mask>, <sent0>',
+        #'template': '<sent0> All in all, it was <mask>.',
+        #'label_words':[['Ġfantastic', 'Ġwonderful', 'Ġamazing', 'Ġexcellent'], ['Ġterrible', 'Ġbad', 'Ġawful', 'Ġhorrible']],
+        #'label_words':[['Ġgreat','Ġexcellent',], ['Ġterrible', 'Ġhorrible',]],
+        #'label_words': [['Ġgreat'], ['Ġterrible']]
+        'label_words':[['Ġwonderful','Ġamazing',], ['Ġterrible', 'Ġbad',]],
+        'dataset_column': ['sentiment', ['review']], #[label name is csv, [features in csv]]
         'metrics':['acc']
     },
 
+    'sst2':{
+        'num_label':2,
+        #'label_ids':{'world':1, 'sport':2, 'business':3, 'sci/tech':4},
+        'label_ids':{'0':0, '1':1},
+        'max_len':110 ,
+        #'template': '<sent> It was <mask>.', #to be fix
+        'template': 'the movie is <mask>, <sent>',
+        'label_words': [['irresistible'], ['pathetic']],
+        'dataset_column': ['label', ['sentence']],
+        'metrics':['acc']        
+    },
+
+    'ag_news':{
+        'num_label':4,
+        #'label_ids':{'world':1, 'sport':2, 'business':3, 'sci/tech':4},
+        'label_ids':{1:0, 2:1, 3:2, 4:3},
+        'max_len':128 ,
+        'template': '<sent0> <sent1> This topic is about <mask>.', #to be fix
+        'label_words': [['Ġpolitics'], ['Ġsports'],['Ġbusiness'], ['Ġtechnology']],
+        'dataset_column': ['Class Index', ['Title', 'Description']],
+        'metrics':['acc']
+    },
+
+    'dbpedia':{
+        'num_label':14,
+        #'label_ids':{'world':1, 'sport':2, 'business':3, 'sci/tech':4},
+        'label_ids':{1:0, 2:1, 3:2, 4:3, 5:4, 6:5, 7:6, 8:7, 9:8, 10:9, 11:10, 12:11, 13:12, 14:13},
+        'max_len':128 ,
+        'template': None, #to be fix
+        'label_words': None,
+        'dataset_column': ['label', ['title', 'content']],
+        'metrics':['acc']        
+    },
+
+    'yahoo':{
+        'num_label':10,
+        #'label_ids':{'world':1, 'sport':2, 'business':3, 'sci/tech':4},
+        'label_ids':{'1':0, '2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, '10':9,},
+        'max_len':128 ,
+        'template': None, #to be fix
+        'label_words': None,
+        'dataset_column': ['topic', ['question_title', 'question_content']],
+        'metrics':['acc']        
+    }
 }
 
     
@@ -35,17 +85,19 @@ def set_config_for_task(config):
     #set labels, template
     #set metrics
     task = config.task_name.lower()
-    task_list = ['imdb']
+    task_list = ['imdb', 'sst2', 'dbpedia', 'ag_news', 'yahoo']
     assert task in task_list, "task '{}' not seen, use task in {} or modify code for new task".format(task, task_list)
     dict_task = task_dict[task]
-    if config.metrics is None: config.metrics = dict_task['metrics']
-    if config.num_label is None: config.num_label = dict_task['num_label']
-    if config.max_len is None: config.max_len = dict_task['max_len']
-    if config.template is None: config.template = dict_task['template']
-    if config.label_words is None: 
+    if hasattr(config, 'metrics'): config.metrics = dict_task['metrics']
+    if hasattr(config, 'num_label'): config.num_label = dict_task['num_label']
+    if hasattr(config, 'max_len'): config.max_len = dict_task['max_len']
+    if hasattr(config, 'template') and config.template is None: config.template = dict_task['template']
+    if hasattr(config, 'label_words') and config.label_words is None: 
         num_label_words = min(config.num_label_words, len(dict_task['label_words']))
         config.label_words = [words[:num_label_words] for words in dict_task['label_words']]
-    if config.label_ids is None: config.label_ids = dict_task['label_ids']
+    if  hasattr(config, 'label_ids'): config.label_ids = dict_task['label_ids']
+    if hasattr(config, 'dataset_column'): config.dataset_column = dict_task['dataset_column']
+    
     
 def compute_metrics(preds, labels, metrics):
     result = None
